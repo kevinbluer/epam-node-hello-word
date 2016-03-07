f8c0d8574cac2d9d98ff3ea8616846db7bc28142
Harry Ng <harryworld@gmail.com>
2016-03-07 16:52:38 +0800

    Add getting article

--- routes/api.js
+++ routes/api.js
@@ -20,16 +20,12 @@ router.get('/articles', function(req, res, next) {
 
 router.get('/articles/:id', function(req, res, next) {
 
-	var fs = require('fs');
-	var obj;
-	fs.readFile('./data/articles.json', 'utf8', function (err, data) {
-		if (err) throw err;
-
-		data = _.filter(JSON.parse(data), function(item) {
-		    return item.id == req.params.id;
-		});
-
-		res.json(data);
+	Article.findById(req.params.id, function(err, article) {
+		if (!err) {
+			res.json(article);
+		} else {
+			res.send(404, 'File not Found.');
+		}
 	});
 });
 
